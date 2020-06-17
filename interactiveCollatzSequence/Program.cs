20ca899dd4a1d0c51ea28b297e98c1ce8c2e15ee
Luis Gomez <luey714@gmail.com>
2020-06-17 07:05:57 -0700

    Completed 3 challenge programs

---
+++ interactiveCollatzSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace interactiveCollatzSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number!");
+            int collatzNumber = int.Parse(Console.ReadLine());
+
+            while(collatzNumber > 1)
+            {
+                if(collatzNumber % 2 == 0)
+                {
+                    collatzNumber /= 2;
+                    Console.WriteLine(collatzNumber);
+                }
+                else 
+                {
+                    collatzNumber = collatzNumber * 3 + 1;
+                    Console.WriteLine(collatzNumber);
+                }
+            }    
+
+        }
+    }
+}
